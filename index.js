23af40d096b61aebe0cbae662a7bde791b6f1bea
bengarrett <code.by.ben@gmail.com>
2019-02-21 09:51:03 +1100

    Help menu activated

--- index.js
+++ index.js
@@ -7,9 +7,17 @@
 (function() {
     const hw = document.getElementById(`hardware`)
     const opt = document.getElementById(`options`)
+    const hlp = document.getElementById(`helpTab`)
     const hwBut = document.getElementById(`hwButton`)
     const optBut = document.getElementById(`optButton`)
+    const hlpBut = document.getElementById(`hlpButton`)
+    const resetMenu = function() {
+        hw.style.display = `none`
+        opt.style.display = `none`
+    }
 
+    resetMenu()
+    hw.style.display = `block`
     console.info(`index.js v1.0 loaded`)
     //        document.getElementById(`hardware`)
     hwBut.onclick = function() {
@@ -20,9 +28,8 @@
         resetMenu()
         opt.style.display = `block`
     }
-
-    const resetMenu = function() {
-        hw.style.display = `none`
-        opt.style.display = `none`
+    hlpBut.onclick = function() {
+        resetMenu()
+        hlp.style.display = `block`
     }
 })()
