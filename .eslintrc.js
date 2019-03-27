a37f42f8e922f706f7749a4b4905813892e355cb
Ben Garrett <code.by.ben@gmail.com>
2019-03-27 13:21:02 +1100

    Cleaned up globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,38 @@
 /* spell-checker: disable */
 module.exports = {
-    "env": {
-        "browser":true,
-        "es6": true,
+    env: {
+        browser: true,
+        es6: true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2017,
-        "impliedStrict": true,
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 2017,
+        impliedStrict: true
     },
-    "rules": {
-        "indent": ["error", 4, { "SwitchCase": 1 }],
-        "linebreak-style": [
-            "off"
-        ],
+    rules: {
+        indent: ["error", 4, { SwitchCase: 1 }],
+        "linebreak-style": ["off"],
         "no-console": ["error", { allow: ["log", "error", "warn"] }],
-        "no-fallthrough": ["error", { "commentPattern": "break[\\s\\w]*omitted" }],
-        "no-unused-vars": ["error", { "vars": "local" }],
-        "quotes": [
+        "no-fallthrough": [
             "error",
-            "backtick",
+            { commentPattern: "break[\\s\\w]*omitted" }
         ],
-        "semi": [
-            "error",
-            "never",
-        ],
-        "strict": ["error", "global"],
+        "no-unused-vars": ["error", { vars: "local" }],
+        quotes: ["error", "backtick"],
+        semi: ["error", "never"],
+        strict: ["error", "global"]
     },
-    "globals": {
-        "DoseeLoader": true,
-        "Emulator": true,
-        "getMetaContent": true,
-        "storageAvailable": true,
-
-        "BuildEcma48": true,
-        "buildLinksToCSS": true,
-        "changeTextScanlines": true,
-        "changeTextEffect": true,
-        "checkArg": true,
-        "checkErr": true,
-        "checkRange": true,
-        "displayErr": true,
-        "findControlSequences": true,
-        "findEngine": true,
-        "Font": true,
-        "Guess": true,
-        "humaniseFS": true,
-        "Palette": true,
-        "ParseToChildren": true,
-        "runSpinLoader": true,
-        "Transcode": true,
+    globals: {
+        BrowserFS: true,
+        DoseeLoader: true,
+        Emulator: true,
+        ES6Promise: true,
+        FS: true,
+        getMetaContent: true,
+        Module: true,
+        newQueryString: true,
+        Promise: true,
+        setMetaContent: true,
+        storageAvailable: true
     }
-};
+}
