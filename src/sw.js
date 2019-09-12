cfd64a850abdc6e598e0a6ac1953b9b5b731b1ae
Ben Garrett <code.by.ben@gmail.com>
2019-09-12 12:57:33 +1000

    refactored dependencies location

--- src/sw.js
+++ src/sw.js
@@ -7,6 +7,17 @@ if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`)
 
     workbox.precaching.precacheAndRoute([])
+
+    workbox.routing.registerRoute(/(.*)article(.*)\.html/, args => {
+        return articleHandler.handle(args).then(response => {
+            if (!response) {
+                return caches.match(`offline.html`)
+            } else if (response.status === 404) {
+                return caches.match(`404.html`)
+            }
+            return response
+        })
+    })
 } else {
     console.log(`Boo! Workbox didn't load 😬`)
 }
--- workbox-config.js
+++ workbox-config.js
@@ -3,6 +3,7 @@ module.exports = {
     globDirectory: `build/`,
     globPatterns: [`**/*.{css,zip,json,js,mem,png,html}`],
     maximumFileSizeToCacheInBytes: `8000000`,
-    swDest: `build\\sw.js`,
-    swSrc: `src/sw.js`
+    swDest: `build/sw.js`,
+    swSrc: `src/sw.js`,
+    globIgnores: ["../workbox-config.js"]
 }
