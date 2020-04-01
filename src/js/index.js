bbb961a27d21aa0fd61e7271703d250bbe16c531
Ben Garrett <code.by.ben@gmail.com>
2020-04-01 12:39:23 +1100

    fixed broken menu buttons

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,39 @@
  */
 "use strict"
 ;(() => {
+  // Menu containers
+  const menuTabs = new Map()
+    .set(`hardware`, document.getElementById(`hardwareTab`))
+    .set(`options`, document.getElementById(`optionsTab`))
+    .set(`help`, document.getElementById(`helpTab`))
+
+  // Menu buttons
+  const menuBtns = new Map()
+    .set(`hardware`, document.getElementById(`hardwareBtn`))
+    .set(`options`, document.getElementById(`optionsBtn`))
+    .set(`help`, document.getElementById(`helpBtn`))
+
+  // Create mouse click events for each menu button
+  function monitorTabs() {
+    menuBtns.forEach(button => {
+      if (button === null) return
+      button.addEventListener(`click`, event => {
+        const srcId = event.srcElement.id
+        // replace the trailing `Btn` from the srcElement Id with `Tab`
+        const newId = `${srcId.slice(0, -3)}Tab`
+        resetTabs(newId)
+      })
+    })
+  }
+
+  // Hides all menu containers, if a defaultTab is provided then it will be displayed
+  function resetTabs(defaultTab) {
+    menuTabs.forEach(tab => {
+      if (tab.id === `${defaultTab}`) tab.classList.remove(`hidden`)
+      else tab.classList.add(`hidden`)
+    })
+  }
+
   monitorTabs()
   resetTabs(`hardwareTab`)
   // set the <H2> element to show the running program and archive filename
@@ -60,36 +93,3 @@
   }
   console.log(`Loaded index.js`)
 })()
-
-// Menu containers
-const menuTabs = new Map()
-  .set(`hardware`, document.getElementById(`hardwareTab`))
-  .set(`options`, document.getElementById(`optionsTab`))
-  .set(`help`, document.getElementById(`helpTab`))
-
-// Menu buttons
-const menuBtns = new Map()
-  .set(`hardware`, document.getElementById(`hardwareBtn`))
-  .set(`options`, document.getElementById(`optionsBtn`))
-  .set(`help`, document.getElementById(`helpBtn`))
-
-// Create mouse click events for each menu button
-function monitorTabs() {
-  menuBtns.forEach(button => {
-    if (button === null) return
-    button.addEventListener(`click`, event => {
-      const srcId = event.srcElement.id
-      // replace the trailing `Btn` from the srcElement Id with `Tab`
-      const newId = `${srcId.slice(0, -3)}Tab`
-      resetTabs(newId)
-    })
-  })
-}
-
-// Hides all menu containers, if a defaultTab is provided then it will be displayed
-function resetTabs(defaultTab) {
-  menuTabs.forEach(tab => {
-    if (tab.id === `${defaultTab}`) tab.classList.remove(`hidden`)
-    else tab.classList.add(`hidden`)
-  })
-}
