844612c9bf93da3286eec666d9ddc801b6070155
bengarrett <code.by.ben@gmail.com>
2022-04-23 10:26:38 +1000

    Fixed invalid type. Workbox would not build do to the maximumFileSizeToCacheInBytes value.

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: `build/`,
   globPatterns: [`**/*.{css,html,ico,js,json,mem,png,wasm,zip}`],
-  maximumFileSizeToCacheInBytes: `8000000`,
+  maximumFileSizeToCacheInBytes: 8000000,
   swDest: `build/sw.js`,
   swSrc: `src/sw.js`,
   globIgnores: [`../workbox-config.js`],
